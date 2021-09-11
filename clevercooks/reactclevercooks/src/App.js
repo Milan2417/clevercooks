5a70f2b79830d09711ea9f5f05f61e0966d15b62
Milan2417 <81343595+Milan2417@users.noreply.github.com>
2021-09-11 14:54:12 +0530

    first commit

---
+++ clevercooks/reactclevercooks/src/App.js
@@ -0,0 +1,43 @@
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="center-column">
+        <div className="item-row">
+          <span>One</span>
+        </div>
+        <div className="item-row">
+          <span>Two</span>
+        </div>
+        <div className="item-row">
+          <span>Three</span>
+        </div>
+        <div className="item-row">
+          <span>four</span>
+        </div>
+        <div className="item-row">
+          <span>five</span>
+        </div>
+        <div className="item-row">
+          <span>Six</span>
+        </div>
+        <div className="item-row">
+          <span>Seven</span>
+        </div>
+        <div className="item-row">
+          <span>Eight</span>
+        </div>
+        <div className="item-row">
+          <span>Nine</span>
+        </div>
+        <div className="item-row">
+          <span>Ten</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
